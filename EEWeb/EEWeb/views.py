5f9c5908bf53c6c3d812241a4e00abdbfbfcdcad
Knapoj Chaimanekorn <63010104@kmitl.ac.th>
2021-06-06 15:03:09 +0700

    Models Update

--- EEWeb/EEWeb/views.py
+++ EEWeb/EEWeb/views.py
@@ -1,3 +1,7 @@
 from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = "index.html"
+    def get_context_data(self,*args, **kwargs):
+        context = super(IndexView, self).get_context_data(*args,**kwargs)
+        context['title_name'] = 'EE KMITL'
+        return context
--- EEWeb/site_auth/models.py
+++ EEWeb/site_auth/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class EEUserProfile(models.Model):
+    gender_choices = [
+        ('นาย', 'นาย'),
+        ('นางสาว', 'นางสาว'),
+    ]
+    class Meta:
+        db_table = 'auth_userprofile'
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gender = models.CharField(choices=gender_choices, max_length=6, null=True)
+    name = models.CharField(max_length=100,null=True)
+    surname = models.CharField(max_length=100,null=True)
+    nickname = models.CharField(max_length=30,null=True)
+    student_id = models.IntegerField(null=True)
+    birth_date = models.DateField(null=True)
+    address = models.CharField(max_length=1000, null=True)
+    self_telephone_num = models.CharField(max_length=10, null=True)
+    line_id = models.CharField(max_length=100, null=True)
+    facebook = models.CharField(max_length=100, null=True)
+    instagram = models.CharField(max_length=100, null=True)
+    def __str__(self):
+        return self.name()
