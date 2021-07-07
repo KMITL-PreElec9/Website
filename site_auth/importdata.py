feceabbfeaf8982a6b549e6c342e7bd1040ecb55
Knapoj Chaimanekorn <63010104@kmitl.ac.th>
2021-07-07 11:26:48 +0700

    edit importdata

--- site_auth/importdata.py
+++ site_auth/importdata.py
@@ -1,4 +1,5 @@
 #%%
+from numpy import NaN, nan
 from .models import *
 import pandas as pd
 import datetime
@@ -12,13 +13,15 @@ def cvtime(oldtime):
 fields = ["ชื่อ",'นามสกุล','รหัส','เบอร์โทร','ไอดีไลน์','วันเกิด','ชื่อเล่น', 'อีเมล', 'อีเมล2']
 
 data = pd.read_csv ('data/electronics_64.csv')   
-#EEData_64.objects.all().delete()
+EEData_64.objects.all().delete()
 for j in range(0,len(data)):
     db = EEData_64( 
         name = data[fields[0]][j],surname = data[fields[1]][j],
         student_id = data[fields[2]][j],
-        self_telephone_num ='0'+str(data[fields[3]][j]), 
         line_id = data[fields[4]][j], birth_date = cvtime(data[fields[5]][j]),
-         nickname = data[fields[6]][j],email = data[fields[7]][j],email2 = data[fields[7]][j])
+        nickname = data[fields[6]][j],email = data[fields[7]][j],email2 = data[fields[7]][j])
+    if  not pd.isna(data[fields[3]][j]):
+        db.self_telephone_num ='0'+str(int(data[fields[3]][j]))
+    else: db.self_telephone_num = None
     db.save()
 # %%
